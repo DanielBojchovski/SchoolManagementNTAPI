d301d9054e92d8a202039aadf6e5a4826ea94d62
Your Name <you@example.com>
2023-11-03 08:46:48 +0100

    enable load related data

--- SchoolManagementNTAPI/Program.cs
+++ SchoolManagementNTAPI/Program.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMvc(option => option.EnableEndpointRouting = false)
+    .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);  //enables load related data
 
 var app = builder.Build();
 
